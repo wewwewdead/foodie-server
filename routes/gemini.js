b2bb083a1d02a479878014e563f82e131f3beb78
john mathew <johnmathewloren27@gmail.com>
2025-04-26 15:53:35 +0900

    add genAI

--- routes/gemini.js
+++ routes/gemini.js
@@ -1,4 +1,4 @@
-import { GoogleGenAI, Type } from "@google/genai";
+import { GoogleGenAI } from "@google/genai";
 import multer from "multer";
 import express from "express";
 import 'dotenv/config';

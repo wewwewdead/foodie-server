ea0fb68999c5d61696711c7e209087e3fbbca774
john mathew <johnmathewloren27@gmail.com>
2025-05-02 20:05:05 +0900

    added total

--- routes/gemini.js
+++ routes/gemini.js
@@ -167,6 +167,16 @@ router.get('/getFoodLogs', async(req, res) => {
     console.error('error fetchin data from foodlogs', error)
     return;
   }
-  res.json({data});
+
+  const totals = data.reduce((acc, item) => {
+    acc.totalCalories += item.calories || 0;
+    acc.totalCarbs += item.carbs || 0;
+    acc.totalSugar += item.sugar || 0;
+
+    return acc;
+
+  }, {totalCalories: 0, totalCarbs: 0, totalSugar: 0})
+  
+  res.json({data, totals});
 })
 export default router;
